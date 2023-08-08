dd8c1cea54f19037d6294f51728f452b8401eec7
SWINCARD <141781418+SWINCARD@users.noreply.github.com>
2023-08-08 18:05:40 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click', function() {
+    alert('Sabia que dirias que Si, Te quiero. Ahora En geogebra 3D coloca --->   a(x,y)=(((x^(2)+((9)/(80)) y^(2))^(((1)/(2)))+(-3 x^(2)-((729)/(80)) y^(2)+4)^(((1)/(2))))/(2))-0.02 y tambien b(x,y)=(((x^(2)+((9)/(80)) y^(2))^(((1)/(2)))-(-3 x^(2)-((729)/(80)) y^(2)+4)^(((1)/(2))))/(2))')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function() {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top', randomY+'%');
+    noBtn.style.setProperty('left', randomX+'%');
+    noBtn.style.setProperty('transform', 'translate(-${randomX}%, -${randomY}%)');
+})
